54eb7c05bbe6965d0ba1dbefebb0a93392b37af3
Charlemon23 <158347598+Charlemon23@users.noreply.github.com>
2025-10-26 20:55:01 -0500

    Delete data/contracts/offline_seed directory

--- data/contracts/offline_seed/GasDoS.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-contract GasDoS {
-    uint[] public data;
-    function add(uint n) public {
-        for (uint i = 0; i < n; i++) data.push(i);
-    }
-}
